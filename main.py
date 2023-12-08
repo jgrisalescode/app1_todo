6a66695a6ad05f4a1c1e764a6f5a2613c24a9d9d
Julian Grisales <jgrisales@gmail.com>
2023-12-08 18:24:25 +0100

    Day 3 Take decisions

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-user_prompt = "Enter a todo: "
-
 todos = []
 
 while True:
-    todo = input(user_prompt)
-    todos.append(todo)
-    print(todos)
+    user_action = input('Type "add", "show" or "exit": ')
+
+    match user_action: # match only for python 3.10+
+        case 'add':
+            todo = input("Enter a todo: ")
+            todos.append(todo)
+        case 'show':
+            print(todos)
+        case 'exit':
+            break
+
+print("Bye!")
